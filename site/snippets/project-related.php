54757d9534d20f1e9c39702ff2f820c2e0a9fe12
ps <p@brcp.at>
2019-02-09 17:50:26 +0100

    extend the "project" page model by the "related" method

--- site/snippets/project-related.php
+++ site/snippets/project-related.php
@@ -1,5 +1,3 @@
-<? $related_pages = (isset($related_pages) && $related_pages->count() > 1) ? $related_pages : $site->find('projects')->children()->not($page)->not($page->nextVisible())->shuffle()->limit(3) ?>
-
 <section class="bg-greylighter u-pv2">
   <div>
     <p style="margin: 4rem 0 1rem; font-size: 2rem;">related projects</p>
@@ -7,7 +5,7 @@
   
   <!-- Project Cards -->
   <div class="card-container owl-carousel u-mt1">
-    <? foreach ($related_pages as $project) : ?>
+    <? foreach ($page->related() as $project) : ?>
 
       <a href="<?php echo $project->url() ?>" class="card card--related">
         <? if($image = $project->featuredimage()->toFile()) : ?>
