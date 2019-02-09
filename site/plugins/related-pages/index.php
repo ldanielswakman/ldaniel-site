27940b7e09beb4d27b255a246e87469041834404
Pedro Borges <oi@pedroborg.es>
2019-02-09 10:09:08 -0200

    Add 'limit' option
    This allows you to set a global limit for related pages in the config file:
    
    ```php
    // site/config/config.php
    
    return [
        'ldaniel.relatedpages.limit' => 4
    ];
    ```
    
    Or a local limit when the page method is used:
    
    ```php
    $page->related(4);
    ```

--- site/plugins/related-pages/index.php
+++ site/plugins/related-pages/index.php
@@ -2,10 +2,19 @@
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-Kirby::plugin('ldaniel/related-pages', [
+use Kirby\Cms\Pages;
+Kirby::plugin('ldaniel/relatedpages', [
+    'options' => [
+        'limit' => 3
+    ],
     'pageMethods' => [
-        'related' => function () {
-            return $this->siblings()->not($this->nextVisible())->shuffle()->limit(3);
+        'related' => function (int $limit = 0): Pages {
+            $limit = $limit > 0 ? $limit : option('ldaniel.relatedpages.limit');
+
+            return $this->siblings()
+                ->not($this->nextVisible())
+                ->shuffle()
+                ->limit($limit);
         }
     ]
 ]);
